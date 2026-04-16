9a35f24e2fe278888443bc57c9559e5d29f9fcd5
Ladji-tim33 <ladjitimer52@gmail.com>
2026-04-16 21:25:22 +0000

    feedback

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -6,7 +6,7 @@ import Header from "../shared/components/header"
 
 
 export default function DashboardLayout() {
-  const [open, setOpen] = useState(true)
+  const [open, setOpen] = useState(false)
   return (
     <div className="min-h-screen bg-[var(--color-background)]">
       <Sidebar collapsed={!open} />
